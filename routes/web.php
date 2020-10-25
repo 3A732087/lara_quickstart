8c6a09a1ad663b18d4606446c157bd034288b323
Xu jia quan <quannn0180716v@gmail.com>
2020-10-25 20:30:44 +0800

    第二個路由：資料驗證與錯誤訊息

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,16 @@ Route::get('/',function (){
 
 //增加新的任務
 Route::post('/task',function (Request $request){
+    $validator = Validator::make($request->all().[
+        'name'=>'required|max:255',
+        ]);
+
+    if($validator->fails()){
+        return  redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
     $task = new Task;
     $task->name = $request->name;
     $task->save();
